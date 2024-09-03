803dc3f51fb92efce8893c9f0a78326428365dae
Ashik-Akash <ashikikbalakash15@gmail.com>
2024-09-03 09:45:21 +0600

    initial commit

--- datetime.php
+++ datetime.php
@@ -1,17 +1,15 @@
 <?php
 //Formatting a Date String
 
-$day = 5;
 $h = 10;
 $m = 00;
 $s = 55;
+$day = 5;
 $month = "September";
 $year = 2024;
 
-$time = $h . ':' . $m . ':' .$s;
-
-//echo $time;
-echo $alldatetime = $time." Date is : ". $day ." Month is : ". $month . " Year is :  ". $year;
+$time = $h . ':' . $m . ':' . $s;
+echo $alldatetime = $time." Date is : ". $day ." Month is : ". $month . " Year is : ". $year;
 
 ?>
 
