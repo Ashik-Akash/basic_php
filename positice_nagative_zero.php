335c5c3bc1c22923e34450600caad7041084996a
Ashik-Akash <ashikikbalakash15@gmail.com>
2024-09-07 00:07:50 +0600

    Frist commit

---
+++ positice_nagative_zero.php
@@ -0,0 +1,15 @@
+<?php
+
+$num = 9;
+
+if ($num > 0){
+    echo "This is a positive number";
+}
+else if ($num < 0) {
+    echo "This is nagative number";
+}
+else {
+    echo "This is zero number";
+}
+
+?>
