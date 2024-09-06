977f513a854513124487a5e550c2eb0544c438f7
Ashik-Akash <ashikikbalakash15@gmail.com>
2024-09-06 23:11:13 +0600

    initial commit

--- evenodd.php
+++ evenodd.php
@@ -1,13 +1,14 @@
 <?php 
  //check even and odd number
 
-$number = 11;
+$number = 1;
 
 if ($number % 2 == 0){
 
     echo "This number is even number";
 }
 else {
+
     echo "This number is odd number";
 }
 
