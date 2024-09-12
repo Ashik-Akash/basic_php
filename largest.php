8d7f7263f880da47f9e399be11fc15c577ee0d63
Ashik-Akash <ashikikbalakash15@gmail.com>
2024-09-12 21:59:09 +0600

    frist commit

---
+++ largest.php
@@ -0,0 +1,17 @@
+<?php 
+$a = 4;
+$b = 70;
+$c = 33;
+
+if ( $a > $b && $a > $c){
+    echo " a is  largest number";
+}
+else if ($b > $c && $b > $c){
+    echo "b is largest number";
+}
+
+
+
+
+
+?>
