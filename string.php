3e083ec2f1820f45289428b2b1f1b4ac8e76b324
Ashik-Akash <ashikikbalakash15@gmail.com>
2024-09-15 10:47:27 +0600

    initial commit

--- string.php
+++ string.php
@@ -3,6 +3,7 @@
 
 $name = "Ashik Ikbal Akash";
 $age = 27;
+$birthdate = 1997;
 
-echo 'My name is' .$name. ' and age is ' .$age;
+echo 'My name is' .$name. ' and age is ' .$age.'Birthdate is '.$birthdate;
 ?>
