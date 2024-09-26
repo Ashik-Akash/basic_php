fbecae0f6a31bd0b6e69555417ab7a86eac774b9
Ashik-Akash <ashikikbalakash15@gmail.com>
2024-09-26 23:50:20 +0600

    initial commit

--- evenodd.php
+++ evenodd.php
@@ -1,11 +1,11 @@
 <?php 
  //check even and odd number
 
-$number = 9;
+$number = 1999;
 
 if ($number % 2 == 0){
 
-    echo "This number is Even  number";
+    echo "This number is Even number";
 }
 else {
 
